aa9655c98bb8bc8aa461d854e5b087319ee9ca6a
moonmoony <moonyinseoul@gmail.com>
2021-09-04 14:39:49 +0900

    비밀번호 암호화 후 저장

--- config/key.js
+++ config/key.js
@@ -1,3 +1,8 @@
+/**
+ * 환경변수 process.env.NODE_ENV
+ * Local 환경 : development / dev.js
+ * Deploy 환경 : production / prod.js
+ */
 if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod')
 } else {
--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,7 @@
+/**
+ * Deploy 환경 : production / prod.js
+ * dashboard.heroku.com
+ */
 module.exports = {
     mongoURI : process.env.MONGO_URI
 }
