57d9fe49989b76acda65fb702ca68787398a6240
moonmoony <moonyinseoul@gmail.com>
2021-09-05 15:01:26 +0900

    토큰으로 비교하여 로그인

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const app = express()
 const port = 1227
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 const { User } = require("./models/User");
 
+//application/x-www-form-urlencoded 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI)
@@ -24,6 +28,52 @@ app.post('/register', (req, res) => {
         if(err) return res.json({ success: false, err })
         return res.status(200).json({
             success: true
+        })
+    })
+})
+
+/**
+ * 로그인 테스트 방법
+ * mongoDB 사이트에서 확인되는 로그인 정보 id(moonmoony@gmail.com) pw(123456789) 를 가지고
+ * postman 사이트에서 post 보내고 response 확인하기
+ */
+app.post('/login', (req, res) => {
+    console.log('*** [index.js] starts to sign in ***')
+
+    //클라이언트에서 요청된 로그인 정보를 db에 있는지 찾기
+    User.findOne({ email: req.body.email }, (err, user) => {
+        console.log('*** [index.js] user check: ', user)
+
+        if(!user) {
+            return req.json({
+                loginSuccess: false,
+                message: "*** There is no user ***"
+            })
+        }
+
+        //이메일 확인 후 맞으면 패스워드 확인
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            console.log('*** [index.js] err 확인: ', err)
+            console.log('*** [index.js] isMatch t/f 확인: ', isMatch)
+
+            if(!isMatch)
+            return res.json({ loginSuccess: false, message: "*** It is no match password ***"})
+
+            //패스워드 확인 후 토큰 생성 https://www.npmjs.com/package/jsonwebtoken 참고
+            user.generateToken((err, user) => {
+                console.log('*** [index.js] user check: ', user)
+                if(err) return res.status(400).send(err);
+
+                //토큰을 저장하는데 어디에 저장할지 설정 => >>>쿠키<<< or 로컬스토리지 or 세션
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({ 
+                    loginSuccess: true, 
+                    userId: user._id,
+                    email: user.email,
+                    message: "로그인에 성공하였습니다." 
+                })
+            })
         })
     })
 })
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -47,8 +48,52 @@ userSchema.pre('save', function( next ) {
                 next()
             })
         })
+    } else {
+        next()
     }
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    //plainPassword와 db에 들어있는 암호화된 pw를 비교
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err);
+        cb(null, isMatch);
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    //es5문법으로 user정보 가져오기
+    var user = this;
+    console.log('*** [User.js] user._id 확인: ', user._id)
+
+    //jsonwebtoken을 이용해서 로그인 정보 일치에 대한 token 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    
+    /**
+     * user._id + 'secretToken' = token
+     * 따라서 나중에는 secretToken -> user._id가 됨
+     */
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        //유저 아이디로 유저를 찾은 후 클라이언트에서 보낸 token과 db에 보관된 token 일치 여부 확인
+        user.findOne({
+            "_id": decoded, 
+            "token": token
+        }, function(err, user) {
+            if(err) return cb(err)
+            cb(null, user)
+        })
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
